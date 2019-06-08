16daf233634e2a6e9c49e16c0b672527332ba89c
krogank9 <wifi.mouse.app@gmail.com>
2019-06-08 19:58:52 -0400

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Add a shopping list element customized with the given name
 function addItem(name) {
 	$(".shopping-list").append(`
 	<li>
@@ -14,6 +15,8 @@ function addItem(name) {
 	`);
 }
 
+// When form is submitted, preventDefault to block submission to the server
+// and add the an item to the shopping list with the value of the shopping-list-entry input
 $("#js-shopping-list-form").submit(function(e) {
 	e.preventDefault();
 	let entry = $("#shopping-list-entry").val();
@@ -21,10 +24,14 @@ $("#js-shopping-list-form").submit(function(e) {
 		addItem(entry);
 })
 
+// Employ event delegation for these next two shopping item events, as items will be removed & added:
+
+// Upon clicking the check button, toggle the strike through styling w/ toggleClass
 $(".shopping-list").on("click", ".shopping-item-toggle", function(){
 	$(this).closest("li").children().first().toggleClass("shopping-item__checked");
 });
 
+// Remove the parent li when the delete button inside of it is pressed
 $(".shopping-list").on("click", ".shopping-item-delete", function(){
 	$(this).closest("li").remove();
 });
